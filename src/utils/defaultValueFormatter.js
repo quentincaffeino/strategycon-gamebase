033e5730557e8e163fd64a714d07edec766dfdd7
Sergei ZH <sergey95zu@gmail.com>
2020-06-28 00:02:46 +0200

    Updated default value formatter

--- src/utils/defaultValueFormatter.js
+++ src/utils/defaultValueFormatter.js
@@ -4,9 +4,10 @@ import { getValue } from './getValue'
 
 /**
  * @param {ValueGetterParams} params
+ * @param {string} defaultValue
  * @returns {any}
  */
-export function defaultValueFormatter(params) {
+export function defaultValueFormatter(params, defaultValue = '—') {
   const value = getValue(params)
-  return value || '—'
+  return value || defaultValue
 }
