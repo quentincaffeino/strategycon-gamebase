6dadb6c81ecf3bcca7b8faf47e27c08b4b29a2a4
Sergei ZH <sergey95zu@gmail.com>
2020-09-06 15:06:11 +0000

    Updated steam rating color from blue to green

--- src/columns/thirdPartyRating/helpers.js
+++ src/columns/thirdPartyRating/helpers.js
@@ -1,4 +1,3 @@
-
 import { steamRatingProvider } from "./steamRatingProvider"
 import { opencriticRatingProvider } from "./opencriticRatingProvider"
 import { metacriticRatingProvider } from "./metacriticRatingProvider"
@@ -14,7 +13,7 @@ function getSteamRatingColor(rating) {
   } else if (rating < 70.0) {
     return "orange"
   } else {
-    return "blue"
+    return "green"
   }
 }
 
