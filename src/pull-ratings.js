0415ea36ce6677f170bee40863acd4d9707761eb
Sergei ZH <sergey95zu@gmail.com>
2020-09-22 00:34:28 +0200

    Do not set local storate item if there is no data

--- src/pull-ratings.js
+++ src/pull-ratings.js
@@ -5,7 +5,7 @@ import { localStorageTest } from './utils/localStorageTest'
 fetch('https://gametable.strategycon.ru/gamestable/items/steam_rating')
   .then(res => res.json())
   .then(({ data }) => {
-    if (localStorageTest()) {
+    if (data && localStorageTest()) {
       localStorage.setItem('data-collection-steam_rating', JSON.stringify(data))
     }
   })
