6ea1ae85c2b35abc58ffb5e189ba0026f042b8e3
quentincaffeino <sergey95zu@gmail.com>
2021-03-15 16:59:48 +0100

    Pull steam ratings with pagination (cause it is limited on the server)

--- src/pull-ratings.js
+++ src/pull-ratings.js
@@ -1,12 +1,37 @@
 import { localStorageTest } from "./utils/localStorageTest";
 
-fetch("https://gametable.strategycon.ru/gamestable/items/steam_rating")
-  .then((res) => res.json())
-  .then(({ data }) => {
-    if (data && localStorageTest()) {
+const ITEMS_PER_PAGE = 100;
+
+function pullSteamRatingsPage({ page }) {
+  const url =
+    "https://gametable.strategycon.ru/gamestable/items/steam_rating?limit=" +
+    ITEMS_PER_PAGE +
+    "&page=" +
+    page;
+
+  return fetch(url).then((res) => res.json());
+}
+
+async function pullSteamRatings() {
+  try {
+    let page = 1;
+    let allSteamRatings = [];
+    let steamRatings;
+    do {
+      steamRatings = (await pullSteamRatingsPage({ page })).data;
+      allSteamRatings = allSteamRatings.concat(steamRatings);
+      page++;
+    } while (steamRatings.length >= ITEMS_PER_PAGE);
+
+    if (allSteamRatings.length && localStorageTest()) {
       localStorage.setItem(
         "data-collection-steam_rating",
-        JSON.stringify(data)
+        JSON.stringify(allSteamRatings)
       );
     }
-  });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+pullSteamRatings();
