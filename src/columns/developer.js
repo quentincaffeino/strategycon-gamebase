592738d83d8e2f8d18498da3d19125aa4afc4fb4
ilya53nov <ilya53nov@yandex.ru>
2021-12-11 20:07:44 +0300

    Удалён код для приклеивания ссылок к словам

--- src/columns/developer.js
+++ src/columns/developer.js
@@ -6,24 +6,12 @@ import { getFormattedValue } from "../utils/getValue";
  * @returns {any}
  */
 function cellRenderer(params) {
-  const getLinkTemplateFor = (link, name) =>
-    link
-      ? `<a href="${link}" style="display:inline" rel="noopener noreferrer" target="_blank">${name}</a>`
-      : name;
-
+  
   const names = (getFormattedValue(params) || "")
-    .split(",")
-    .map((el) => el.trim());
+  .split(",")
+  .map((el) => el.trim());
 
-  const links = (params.data.developer_link || "")
-    .split(",")
-    .map((el) => el.trim());
-
-  let result = [];
-  for (let i = 0; i < names.length; ++i) {
-    result.push(getLinkTemplateFor(links[i], names[i]));
-  }
-  return result.join(", ");
+  return names.join(", ");
 }
 
 export const field = {
